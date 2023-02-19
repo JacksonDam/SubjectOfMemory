2aafa6b6c134b8b330f241b625347f5c50a1219f
Jackson Dam <2619114D@student.gla.ac.uk>
2023-02-19 03:00:19 +0000

    hm

--- memory.py
+++ memory.py
@@ -25,11 +25,15 @@ def memory(array_of_arrays):
     def stage_three(array):
         prompt = array[-1]
         if prompt == 1:
-            positions.append(1)
-            return pressed[1]
+            label = int(pressed[1])
+            same_lab_pos = array.index(label)
+            positions.append(same_lab_pos)
+            return str(array[same_lab_pos])
         elif prompt == 2:
-            positions.append(0)
-            return pressed[0]
+            label = int(pressed[0])
+            same_lab_pos = array.index(label)
+            positions.append(same_lab_pos)
+            return str(array[same_lab_pos])
         elif prompt == 3:
             positions.append(2)
             return str(array[2])
@@ -50,14 +54,21 @@ def memory(array_of_arrays):
             return str(array[positions[1]])
 
     def stage_five(array):
-        return str(array[positions[array[-1] - 1]])
+        prompt = array[-1]
+        if prompt == 1:
+            return str(array[positions[0]])
+        elif prompt == 2:
+            return str(array[positions[1]])
+        elif prompt == 3:
+            return str(array[positions[2]])
+        elif prompt == 4:
+            return str(array[positions[3]])
 
     pressed.append(stage_one(array_of_arrays[0]))
     pressed.append(stage_two(array_of_arrays[1]))
     pressed.append(stage_three(array_of_arrays[2]))
     pressed.append(stage_four(array_of_arrays[3]))
-    pressed.append(pressed[(array_of_arrays[4][-1]) - 1])
-
+    pressed.append(stage_five(array_of_arrays[4]))
     return "".join(pressed)
 
 # print(memory([[1,3,2,4,1],
